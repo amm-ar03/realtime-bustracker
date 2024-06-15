ae3a82cde076a71bd264f0d84d604cd80c5dba6b
amm-ar03 <ammarpatel2003@gmail.com>
2024-06-15 08:47:41 -0700

    added vehicle postions and routes highlighted

--- live.py
+++ live.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import json
@@ -8,17 +9,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('connect')
 def emit_data():
     while True:
         try:
             with open('gtfs_realtime.json', 'r') as json_file:
                 feed_dict = json.load(json_file)
 
+                socketio.emit('vehicle-update', feed_dict)
                 # Add vehicle positions to the map
                 for entity in feed_dict['entity']:
                     if 'vehicle' in entity:
@@ -29,13 +31,14 @@ def emit_data():
                         if latitude and longitude:
                             route_id = vehicle.get('trip', {}).get('routeId', 'N/A')
                             vehicle_status = vehicle.get('currentStatus', 'UNKNOWN')
-                        emit('vehicle_update', {'latitude' : latitude, 'longitude' : longitude,
-                                                'route_id': route_id}, broadcast=True)
+                            socketio.emit('vehicle_update', {'latitude' : latitude, 'longitude' : longitude,
+                                                'route_id': route_id})
             time.sleep(30)
 
         except Exception as e:
             print(f"Error updating map: {e}")
 
+@socketio.on('connect')
 def connect():
     print("connected")
 
@@ -44,5 +47,5 @@ if __name__ == "__main__":
     data_thread = Thread(target=emit_data)
     data_thread.daemon = True
     data_thread.start()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=3030)
 
