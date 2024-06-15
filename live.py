86fe74a9876a28dc0eb7b9903d4118a0edc6f278
amm-ar03 <ammarpatel2003@gmail.com>
2024-06-14 22:46:59 -0700

    Added socket connection for updates

--- live.py
+++ live.py
@@ -1,52 +1,48 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 import json
-import folium
-from datetime import datetime
-import os
 import time
+from threading import Thread
 
-def update_map(filename):
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def emit_data():
     while True:
         try:
-            with open(filename, 'r') as json_file:
+            with open('gtfs_realtime.json', 'r') as json_file:
                 feed_dict = json.load(json_file)
 
-            m = folium.Map(location=[48.476165, -123.332778], zoom_start=12)
-
-# Add vehicle positions to the map
-            for entity in feed_dict['entity']:
-                if 'vehicle' in entity:
-                    vehicle = entity['vehicle']
-                    position = vehicle.get('position', {})
-                    latitude = position.get('latitude')
-                    longitude = position.get('longitude')
-                    if latitude and longitude:
-                        route_id = vehicle.get('trip', {}).get('routeId', 'N/A')
-                        vehicle_status = vehicle.get('currentStatus', 'UNKNOWN')
-                        if vehicle_status == 'IN_TRANSIT_TO':
-                            icon = folium.Icon(color='blue', icon='arrow')
-                        else:
-                            icon = folium.Icon(color='red', icon='arrow')
-                        folium.Marker([latitude, longitude],
-                          icon=icon, 
-                            popup=f"Vehicle ID: {vehicle['vehicle']['id']}\nRoute ID: {route_id}").add_to(m)
-
-            timestamp = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-            folium.Marker([48.511443, -123.208775], icon=folium.DivIcon(html=f"<div style='font-size: 12pt; font-weight: bold'>Last Updated: {timestamp}</div>"
-                                                                         )).add_to(m)  
-
-            # Save map to an HTML file
-            temp_path = os.path.join(os.getcwd(), 'vehicle_positions_map.html')
-            m.save(temp_path)
-            os.rename(temp_path, html_path)
-
-            print(f"Saved map to {html_path}")
-
+                # Add vehicle positions to the map
+                for entity in feed_dict['entity']:
+                    if 'vehicle' in entity:
+                        vehicle = entity['vehicle']
+                        position = vehicle.get('position', {})
+                        latitude = position.get('latitude')
+                        longitude = position.get('longitude')
+                        if latitude and longitude:
+                            route_id = vehicle.get('trip', {}).get('routeId', 'N/A')
+                            vehicle_status = vehicle.get('currentStatus', 'UNKNOWN')
+                        emit('vehicle_update', {'latitude' : latitude, 'longitude' : longitude,
+                                                'route_id': route_id}, broadcast=True)
             time.sleep(30)
 
         except Exception as e:
             print(f"Error updating map: {e}")
 
+def connect():
+    print("connected")
+
+
 if __name__ == "__main__":
-    html_path = os.path.join(os.getcwd(), 'vehicle_positions_map.html')
-    update_map('gtfs_realtime.json')
+    data_thread = Thread(target=emit_data)
+    data_thread.daemon = True
+    data_thread.start()
+    socketio.run(app, host='0.0.0.0', port=5000)
 
